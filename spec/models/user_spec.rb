795d5ff9fb819a0485b1f27587b49dec64ceb9cc
onitsuyo <oo.smilyyyyy@gmail.com>
2023-04-21 16:56:46 +0900

    ユーザー新規登録できる場合のテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context 'ユーザー新規登録できる場合' do
+      it '必要な情報を入力すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+  end 
 end
