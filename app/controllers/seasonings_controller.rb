71b47315fcb89ea5712fcde6a28f8642ceed30be
onitsuyo <oo.smilyyyyy@gmail.com>
2023-04-25 18:39:36 +0900

    コントローラーに保存機能を追加

--- app/controllers/seasonings_controller.rb
+++ app/controllers/seasonings_controller.rb
@@ -1,2 +1,10 @@
 class SeasoningsController < ApplicationController
+  def create 
+    Seasoning.create(seasoning_params)
+  end
+
+  private
+  def seasoning_params
+    params.require(:seasoning).permit(:name).merge(item_id: params[:item_id])
+  end
 end
