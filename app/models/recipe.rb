1c2503baf154c30bdc549da9952d5b8e0326e7f2
onitsuyo <oo.smilyyyyy@gmail.com>
2023-04-25 17:19:14 +0900

    ingredientsテーブルを追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
-  
   belongs_to :user
   has_one_attached :image
+  has_many :ingredients
 
   validates :name, presence: true
   validates :caption, presence: true
