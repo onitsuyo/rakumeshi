bf16db0d0e7a72f6f6c114d55e8350ee96365831
onitsuyo <oo.smilyyyyy@gmail.com>
2023-04-25 17:39:33 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "recipes#index"
-  resources :recipes, only: [:index, :new, :create]
+  resources :recipes, only: [:index, :new, :create] do
+    resources :ingredients, only: :create 
+    resources :seasonings, only: :create 
+    resources :procedures, only: :create 
+  end
 end
