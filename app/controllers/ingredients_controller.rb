1ca062661e908419a8e3b34879d099812a8fbebb
onitsuyo <oo.smilyyyyy@gmail.com>
2023-04-26 11:45:01 +0900

    コントローラーの記述を削除

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,10 +1,2 @@
 class IngredientsController < ApplicationController
-  def create
-    Ingredient.create(ingredient_params)
-  end 
-  
-  private 
-  def ingredient_params 
-    params.require(:ingredient).permit(:name).merge(recipe_id: params[:recipe_id])
-  end
 end
--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,10 +1,2 @@
 class ProceduresController < ApplicationController
-  def create 
-    Procedure.create(procedure_params)
-  end
-
-  private 
-  def procedure_params 
-    params.require(:procedure).permit(:step).merge(recipe_id: params[:recipe_id])
-  end
 end
